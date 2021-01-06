7304730763f526fb1de80ca812ff4e232b495895
Isaac Simmons <isaac.simmons@gmail.com>
2021-01-06 10:07:44 -0500

    Add environment parsing package

--- src/settings.js
+++ src/settings.js
@@ -1,27 +1,13 @@
-// TODO: swap this out to use environment-parser?
-const requireEnv = (key) => {
-  const value = process.env[key];
-  if (!value) {
-    console.error('Missing required environment value: ' + key);
-    process.exit(1);
-  }
-  return value;
-};
+import { Settings, envType } from 'environment-parser';
 
-const GITHUB_USERNAME = requireEnv('GITHUB_USERNAME');
-const GITHUB_ACCESS_TOKEN = requireEnv('GITHUB_ACCESS_TOKEN');
-const AWS_REGION = requireEnv('AWS_REGION');
-const AWS_BUCKET = requireEnv('AWS_BUCKET');
-const AWS_ACCESS_KEY_ID = requireEnv('AWS_ACCESS_KEY_ID');
-const AWS_SECRET_ACCESS_KEY = requireEnv('AWS_SECRET_ACCESS_KEY');
-const HEALTHCHECK_PING_URL = process.env.HEALTHCHECK_PING_URL || '';
+export const settings = Settings({
+  GITHUB_USERNAME: envType.string(),
+  GITHUB_ACCESS_TOKEN: envType.string(),
 
-export const settings = {
-  GITHUB_USERNAME,
-  GITHUB_ACCESS_TOKEN,
-  AWS_REGION,
-  AWS_BUCKET,
-  AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY,
-  HEALTHCHECK_PING_URL,
-};
+  AWS_REGION: envType.string(),
+  AWS_BUCKET: envType.string(),
+  AWS_ACCESS_KEY_ID: envType.string(),
+  AWS_SECRET_ACCESS_KEY: envType.string(),
+
+  HEALTHCHECK_PING_URL: envType.string({optional: true}),
+});
